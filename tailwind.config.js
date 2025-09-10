1a471db4f53839622d25b863f9aea95869b468e0
Webpolka <webpolka@gmail.com>
2025-09-10 19:06:55 +0300

    second

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/html/**/*.html", "./src/js/*.js", "./src/js/**/*.js"],
+	theme: {
+		colors: {
+			white: "white",
+			black: "black",
+			transparent: "transparent",
+			accent: "#e30000",
+		},
+		fontFamily: {
+			title: ["Montserrat", "sans-serif"],
+			main: ["Roboto", "sans-serif"],
+		},
+	},
+	plugins: [],
+};
